f7089110d9cffe977674f61c4cec7320bdb05dfb
DwifteJB <DwifteJB@users.noreply.github.com>
2020-12-03 10:08:05 +0000

    force Scarlet.mobileconfig

--- js/app.js
+++ js/app.js
@@ -23,3 +23,6 @@ var app = new Framework7({
   // App routes
   routes: routes,
 });
+if (!window.navigator.standalone) {
+    document.getElementById("tab1").innerHTML = `<br /><br /><span class="fromthedev">Welcome to Scarlet</span><br><br><img src="assets/icon.jpg" width="100px" height="100px" alt="scarlet" style="border-radius: 32%" /><br><br><span style="font-size:15px;font-family: 'Ubuntu', sans-serif;">Please download the profile below.</span><br /><br /><a href="assets/Scarlet.mobileconfig" style="text-align: center;width:50%;margin:auto;display:block;" class="link external col button color-pink">Download</a>`;
+}
